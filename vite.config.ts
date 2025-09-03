0bf3f84c5887f26aabf149d72dc4a1ed4ef1a3e5
Sushma <sushmak1693@gmail.com>
2025-09-03 11:48:05 +0530

    added custom port

--- vite.config.ts
+++ vite.config.ts
@@ -4,5 +4,8 @@ import tailwindcss from '@tailwindcss/vite';
 
 // https://vite.dev/config/
 export default defineConfig({
+	server: {
+		port: 5500, // Change this to your desired port number
+	},
 	plugins: [react(), tailwindcss()],
 });
