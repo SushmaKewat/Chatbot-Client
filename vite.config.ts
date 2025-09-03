78c3335f8009dfe61263a875a1bf25e0ec7a5d55
Sushma <sushmak1693@gmail.com>
2025-09-03 12:23:00 +0530

    added favicon and changed app name

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import tailwindcss from '@tailwindcss/vite';
 // https://vite.dev/config/
 export default defineConfig({
 	server: {
-		port: 5500, // Change this to your desired port number
+		port: 5500,
 	},
 	plugins: [react(), tailwindcss()],
 });
